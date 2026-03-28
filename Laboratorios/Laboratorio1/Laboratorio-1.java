da0ce8f171514c0a71b6043223425eb102e48d8f
RIglesias089 <iglesiasricardozhongli@gmail.com>
2026-03-27 18:29:21 -0600

    Docs: inclusion de carpeta de proyectos

---
+++ Laboratorios/Laboratorio1/Laboratorio-1.java
@@ -0,0 +1,12 @@
+/*Realizar un proyecto en java donde tomando de base el diagrama de clasesn, desarrollar un sistema de
+* inventarios, seleccionar 3 metodos para implementar y incorporar un atributo nuevo que no este en el diagrama
+* requisitos:
+* La calase inventarioUSB este ubicada en un paquete llamado modelo
+* La clase Main debera estar ubicada fuera del paquete modelo
+* El sistema debe iniciar con 100 memorias USB
+* No se deben permitir valor negativos al registrar ventas o agregar inventarios
+* No se puden realizar ventas si no hay en stock la cantidad pedida
+* Despues de cada operacion, debe mostrarse el inventario actualizado
+* Se debe implementar validaciones con If
+* El programa debe mostrar su funcionamiento con datos quemados* */
+
