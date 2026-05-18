ec30616c7235afc774ec8882ed7d6a3d2e5c2bca
RIglesias089 <iglesiasricardozhongli@gmail.com>
2026-05-18 08:10:30 -0600

    Feat: Array normal y dinamico

--- Clases/AprendiendoJava/Aprendiendo-Java/src/main/java/org/modelo/POO/Abstraccion.java
+++ Clases/AprendiendoJava/Aprendiendo-Java/src/main/java/org/modelo/POO/Abstraccion.java
@@ -1,6 +1,19 @@
 package org.modelo.POO;
 
 public class Abstraccion {
-    // Busca ocultar la complejidad inecesaria
-   
+    // Busca ocultar la complejidad inecesaria, es muy usada durante el encapsulamiento
+    //podemos verlo de la siguiente manera
+
+    public class persona {
+
+        String nombre;
+        int edad;
+        //aca sabtrae a una persona porque no moldea cosas como
+        //corazon, sangre, adn
+
+        //solo su nombre y su edad
+
+    }
+
+
 }
---
+++ Clases/AprendiendoJava/Aprendiendo-Java/src/main/java/org/modelo/POO/ArraysPOO.java
@@ -0,0 +1,29 @@
+package org.modelo.POO;
+
+import java.util.ArrayList;
+
+public class ArraysPOO {
+    /*Aca veremos cosas muy importantes pues no solo veremos como tal arrays, si no tambien los
+    * hashmap
+    * hashset y el array normal
+    *
+    * Array norma:
+    * este se define de una manera muy facil y es una lista
+    * dinamica de elementos, con dinamica nos referimos a que puede
+    * crecer y decrecer, comparado al array normal que solo puede tener un numero
+    * especifico de espacios y no puede crecer mas
+    *  */
+
+    //array normal
+    int[] numeros = new int[5];
+
+    //ArrayList
+    ArrayList<String> nombres = new ArrayList<>();
+    //Sonde agregamos elementos, eliminamos y cambia automaticamente
+//Para poder usar este necesitamos importar java.util.ArrayList;
+    //Para agregar elementos deberiamos
+    nombres.add("Ricardo");
+    nombres.add("Ana");
+    nombres.add("Mata");
+
+}
